ad43bad3717d725fab284b55ee100ba8abfa7e40
German Yakimov <german13yakimov@gmail.com>
2020-05-01 17:55:18 +0700

    Add CommandHandler.

--- CommandHandler.py
+++ CommandHandler.py
@@ -2,16 +2,15 @@
 class CommandHandler:
     def __init__(self):
         self._required_commands = ['/start', '/help']
+        self._default_answer = "Sorry, I don't know this command."
 
     def handle(self, command_object):
-        chat_id = command_object['message']['from']['id']
         command = command_object['message']['text']
 
-        if command not in self._required_commands:
-            answer = "Sorry, I don't know this command"
-        elif command == '/start':
-            answer = "Welcome!"
-        elif command == '/help':
-            answer = "I would be happy to help, but I can't do anything yet, so there's nothing to help with."
+        if command in self._required_commands:
+            if command == '/start':
+                return "Welcome!"
+            elif command == '/help':
+                return "I would be happy to help, but I can't do anything yet, so there's nothing to help with."
 
-        return answer
+        return self._default_answer
