389d225158f6b99cb51b761ed01eef71d10d04ea
German Yakimov <german13yakimov@gmail.com>
2020-05-05 20:07:36 +0700

    Add logger to Sender.

--- Services/Sender.py
+++ Services/Sender.py
@@ -1,10 +1,13 @@
 import requests
 import json
+import logging
 from pprint import pprint
 
 
 class Sender:
     def __init__(self, access_token):
+        self._logger = logging.getLogger('Engine.Sender')
+
         self._access_token = access_token
         self._requests_url = f'https://api.telegram.org/bot{access_token}/'
 
@@ -15,31 +18,34 @@ class Sender:
 
         self._reply_keyboard_hide = {'hide_keyboard': True}
 
+        self._logger.info('Sender initialized.')
+
     def send_reply_keyboard(self, chat_id, text):
         reply_markup = json.dumps(self._reply_keyboard)
 
         response = requests.post(self._requests_url + 'sendMessage',
                                  {'chat_id': chat_id, 'text': text,
                                   'reply_markup': reply_markup})
-        pprint(response.json())
+
+        self._logger.info(response.json())
 
     def send(self, chat_id, text, reply_keyboard_hide=False):
         if reply_keyboard_hide:
             response = requests.post(self._requests_url + 'sendMessage',
                                      {'chat_id': chat_id, 'text': text,
                                       'reply_markup': json.dumps(self._reply_keyboard_hide)})
-            pprint(response.json())
         else:
             response = requests.post(self._requests_url + 'sendMessage',
                                      {'chat_id': chat_id, 'text': text})
-            pprint(response.json())
+
+        self._logger.info(response.json())
 
     def send_with_reply_markup(self, chat_id, text, reply_markup):
         reply_markup = json.dumps({'inline_keyboard': reply_markup})
         response = requests.post(self._requests_url + 'sendMessage',
                                  {'chat_id': chat_id, 'text': text,
                                   'reply_markup': reply_markup})
-        pprint(response.json())
+        self._logger.info(response.json())
 
     def answer_callback_query(self, chat_id, callback_query_id, text):
         if text:
@@ -49,4 +55,7 @@ class Sender:
             response = requests.post(self._requests_url + 'answerCallbackQuery',
                                      {'chat_id': chat_id, 'callback_query_id': callback_query_id})
 
-        pprint(response.json())
+        self._logger.info(response.json())
+
+# TODO: add Telegram answer logger
+
