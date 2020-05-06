f3a45ed1d167255d0922988d3a7af2c9b7c9f4d8
German Yakimov <german13yakimov@gmail.com>
2020-05-06 22:04:07 +0600

    add test script instead launcher

--- launcher.py
+++ launcher.py
@@ -5,8 +5,26 @@ from pprint import pprint
 from Services.Engine import Engine
 
 
-access_token = os.environ.get('telegram_access_token')
-engine = Engine(access_token)
-engine.launch_hook('https://vm1139999.hl.had.pm')
+# access_token = os.environ.get('telegram_access_token')
+# engine = Engine(access_token)
+# engine.launch_hook('https://vm1139999.hl.had.pm')
 # engine.launch_long_polling()
+
+import socket
+
+sock = socket.socket()
+sock.bind(('', 443))
+sock.listen(1)
+conn, addr = sock.accept()
+
+print('connected: ', addr)
+
+while True:
+    data = conn.recv(1024)
+    print(data)
+    if not data:
+        break
+    conn.send(data.upper())
+
+conn.close()
 
