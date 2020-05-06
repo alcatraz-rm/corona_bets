24ba9029f7122a8700fee8525f050fffbbfd359e
German Yakimov <german13yakimov@gmail.com>
2020-05-06 19:43:42 +0700

    Change hook.

--- launcher.py
+++ launcher.py
@@ -7,6 +7,6 @@ from Services.Engine import Engine
 
 access_token = os.environ.get('telegram_access_token')
 engine = Engine(access_token)
-engine.launch_hook('176.57.71.32')
+engine.launch_hook('vm1139999.hl.had.pm')
 # engine.launch_long_polling()
 
