674889b0dddda508a9c525dc0fce62a3fe57d029
German Yakimov <german13yakimov@gmail.com>
2020-05-06 20:19:33 +0700

    change port.

--- Services/Engine.py
+++ Services/Engine.py
@@ -163,7 +163,7 @@ class Engine:
 
             self._logger.info('Start listening...')
 
-            self._application.listen(80)
+            self._application.listen(8443)
             tornado.ioloop.IOLoop.current().start()
 
         except KeyboardInterrupt:
