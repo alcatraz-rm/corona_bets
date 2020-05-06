04344f2250c350231395a91baf6e4f3ff201f457
German Yakimov <german13yakimov@gmail.com>
2020-05-07 00:01:38 +0600

    Add ngrok addr.

--- launcher.py
+++ launcher.py
@@ -7,6 +7,6 @@ from Services.Engine import Engine
 
 access_token = os.environ.get('telegram_access_token')
 engine = Engine(access_token)
-engine.launch_hook('http://7abaf79a.ngrok.io')
+engine.launch_hook('http://dc46e521.ngrok.io')
 # engine.launch_long_polling()
 
