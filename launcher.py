3df1862214445e1be11db028e28aa78ac2e64619
German Yakimov <german13yakimov@gmail.com>
2020-05-06 22:25:40 +0600

    Return.

--- launcher.py
+++ launcher.py
@@ -1,32 +1,12 @@
 import json
 import os
 from pprint import pprint
-import chardet
 
 from Services.Engine import Engine
 
 
-# access_token = os.environ.get('telegram_access_token')
-# engine = Engine(access_token)
+access_token = os.environ.get('telegram_access_token')
+engine = Engine(access_token)
 # engine.launch_hook('https://vm1139999.hl.had.pm')
-# engine.launch_long_polling()
-
-import socket
-
-sock = socket.socket()
-sock.bind(('', 443))
-sock.listen(1)
-conn, addr = sock.accept()
-
-print('connected: ', addr)
-
-while True:
-    data = conn.recv(1024)
-    print(chardet.detect(data))
-    print(data.decode(encoding='Windows-1252'))
-    if not data:
-        break
-    conn.send(data.upper())
-
-conn.close()
+engine.launch_long_polling()
 
