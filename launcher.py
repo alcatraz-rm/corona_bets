2d7402c0a65d4b1c879d0bef5b3b62522801b023
German Yakimov <german13yakimov@gmail.com>
2020-05-06 19:16:39 +0700

    Delete auth.json using.

--- launcher.py
+++ launcher.py
@@ -5,9 +5,6 @@ from pprint import pprint
 from Services.Engine import Engine
 
 
-with open('auth.json', 'r', encoding='utf-8') as auth_file:
-    data = json.load(auth_file)
-
 access_token = os.environ.get('telegram_access_token')
 engine = Engine(access_token)
 engine.launch_hook('vm1139999.hl.had.pm')
