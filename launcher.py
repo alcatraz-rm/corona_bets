1bd95968cfc9a2ab9a357a6b7eb8fd34dd996190
German Yakimov <german13yakimov@gmail.com>
2020-05-07 10:37:14 +0700

    Change addr.

--- launcher.py
+++ launcher.py
@@ -7,6 +7,6 @@ from Services.Engine import Engine
 
 access_token = os.environ.get('telegram_access_token')
 engine = Engine(access_token)
-engine.launch_hook('vm1139999.hl.had.pm')
+engine.launch_hook('https://vm1139999.hl.had.pm')
 # engine.launch_long_polling()
 
